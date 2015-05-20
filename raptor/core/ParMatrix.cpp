f19848ea3de5dd3b847848b1f2dd9276a567756d
aperson40 <jon.calhoun42@gmail.com>
2015-05-20 13:43:28 -0500

    Add outline of Parallel matrix.

---
+++ raptor/core/ParMatrix.cpp
@@ -0,0 +1,31 @@
+/******** ParMatrix.cpp **********/
+#include "ParMatrix.hpp"
+
+
+ParMatrix::ParMatrix(int _globalRows, int _globalCols, Matrix* _diag)
+{
+    this.globalRows = _globalRows;
+    this.globalCols = _globalCols;
+    this.diag = _diag;
+    this.off = _offd;
+}
+
+ParMatrix::ParMatrix(ParMatrix* A)
+{
+    this.globalRows = _globalRows;
+    this.globalCols = _globalCols;
+    this.diag = _diag; // should we mark as not owning?
+    this.off = _offd;
+}
+
+ParMatrix::~ParMatrix()
+{
+    delete this->diag;
+    delete this->offd;
+}
+
+void ParMatrix::spmv(ParVector* x, ParVector* b, double alpha, double beta)
+{
+    // b = \alpha Ax + \beta b
+
+}
---
+++ raptor/core/ParMatrix.hpp
@@ -0,0 +1,28 @@
+#ifndef PARMATRIX_HPP
+#define PARMATRIX_HPP
+
+
+#include <mpi.h>
+#include <math.h>
+#include <Eigen/Dense>
+using Eigen::VectorXd;
+
+#include "Matrix.hpp"
+
+
+class ParMatrix
+{
+    public:
+        ParMatrix(_GlobRows, _GlobCols, Matrix* _diag, Matrix* _offd);
+        ParMatrix(ParMatrix* A);
+        ~ParMatrix();
+
+        void spmv(ParVector *x, ParVector *b, double alpha, double beta);
+    
+    private:
+        int globalRows;
+        int globalCols;
+        Matrix* diag;
+	    Matrix* offd;
+};
+#endif
