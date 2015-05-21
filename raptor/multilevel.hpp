9d96449fff64227c40e425fa50acaa3f8c55cc5e
Luke Olson <luke.olson@gmail.com>
2015-05-21 11:37:08 -0500

    start multilevel structure

---
+++ raptor/multilevel.hpp
@@ -0,0 +1,48 @@
+#ifndef MULTILEVEL_HPP
+#define MULTILEVEL_HPP
+
+#include <math.h>
+#include <Eigen/Dense>
+#include "ParMatrix.hpp"
+#include "ParVector.hpp"
+
+class level {
+  public:
+    // Constructor
+    level();
+    // Destructor
+    ~level();
+
+    // Data
+    ParCSRMatrix* A;
+    ParCSRMatrix* R;
+    ParCSRMatrix* P;
+}
+
+class multilevel
+{
+    public:
+      // Constructor
+      multilevel(level* levels, void* coarse_solver);
+      // Destructor
+      ~multilevel();
+
+      // print
+      char* doc();
+      double cycle_complexity();
+      double operator_complexity();
+      double grid_complexity();
+
+      ParVector* psolve(const ParVector* b);
+      void solve(const ParVector* b,
+                 const ParVector* x0,
+                 const double tol,
+                 const int maxiter,
+                 const char* cycle,
+                 const char* accel,
+                 double* residuals,
+                 Parvector* x0);
+
+      void* coarse_grid_solver;
+};
+#endif
