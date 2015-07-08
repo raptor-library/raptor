d024ef4ce4cb9651ad0e305569279b1d404448da
aperson40 <jon.calhoun42@gmail.com>
2015-07-08 16:48:33 -0500

    Add IO.

---
+++ raptor/util/tests/test_spmv_IO.cpp
@@ -0,0 +1,39 @@
+#include <mpi.h>
+#include <gtest/gtest.h>
+#include <math.h>
+#include "core/par_matrix.hpp"
+#include "core/par_vector.hpp"
+#include "gallery/matrix_IO.hpp"
+#include "util/linalg/spmv.hpp"
+
+
+TEST(linag, spmv) {
+
+	int rank, num_procs;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+	ParMatrix* A = readParMatrix("file.mtx", MPI_COMM_WORLD, true);
+
+	int global_num_rows = A->global_rows;
+	int local_num_rows = A->local_rows;
+
+	// Create the rhs and solution
+	ParVector* b = new ParVector(global_num_rows, local_num_rows);
+	ParVector* x = new ParVector(global_num_rows, local_num_rows);
+
+	x->set_const_value(1.);
+	parallel_spmv(A, x, b, 1., 0.);
+
+	for (int proc = 0; proc < num_procs; proc++)
+	{
+		if (proc == rank) {
+			for (int i = 0; i < local_num_rows; i++)
+			{
+				double* data = (b->local)->data();
+				printf("b[%d] = %2.3e\n", i+(A->first_col_diag), data[i]);
+			}
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+}
