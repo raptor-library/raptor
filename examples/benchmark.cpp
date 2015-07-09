e3f990bdde756f0a34782ee6e7fe0b66ceb92271
aperson40 <jon.calhoun42@gmail.com>
2015-07-09 11:04:27 -0500

    Add SpMV benchmark.

---
+++ examples/benchmark.cpp
@@ -0,0 +1,61 @@
+#include <mpi.h>
+#include <math.h>
+
+#include "core/par_matrix.hpp"
+#include "core/par_vector.hpp"
+#include "gallery/matrix_IO.hpp"
+#include "util/linalg/spmv.hpp"
+using namespace raptor;
+int main(int argc, char *argv[])
+{
+	int rank, num_procs;
+    
+    MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    // matrix to read
+    char file[] = "./build/test/LFAT5.mtx";
+    //char file[] = "msc01440.mtx";
+    //char file[] = "plbuckle.mtx";
+
+	// Create the matrix, rhs, and solution
+	ParMatrix* A = readParMatrix(file, MPI_COMM_WORLD, true);
+    assert(A != NULL && "Error reading matrix!!!");
+	int global_num_rows = A->global_rows;
+	int local_num_rows = A->local_rows;
+
+	ParVector* b = new ParVector(global_num_rows, local_num_rows);
+	ParVector* x = new ParVector(global_num_rows, local_num_rows);
+	x->set_const_value(1.);
+
+    // Time the SpMV
+	double t0 = MPI_Wtime();
+    parallel_spmv(A, x, b, 1., 0.);
+    double total_time = MPI_Wtime() - t0;
+    MPI_Reduce(&total_time, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+        printf("Max time for SpMV: %g\n", t0);
+    MPI_Reduce(&total_time, &t0, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+        printf("Min time for SpMV: %g\n", t0);
+    MPI_Reduce(&total_time, &t0, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+        printf("Avg time for SpMV: %g\n", t0/num_procs);
+	
+    // Display result vector to verify correctness
+    for (int proc = 0; proc < num_procs; proc++)
+	{
+		if (proc == rank) {
+			for (int i = 0; i < local_num_rows; i++)
+			{
+				double* data = (b->local)->data();
+				printf("b[%d] = %2.3e\n", i+(A->first_col_diag), data[i]);
+			}
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+    MPI_Finalize();
+
+    return 0;
+}
