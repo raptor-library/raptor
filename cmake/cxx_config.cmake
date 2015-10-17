b61b2757a8d06ffebca8b9f71a2a319406d2ed12
Amanda Bienz <bienz2@illinois.edu>
2015-10-17 11:57:48 -0500

    Only creating static libraries -- no need for dynamic on distributed computers

--- cmake/cxx_config.cmake
+++ cmake/cxx_config.cmake
@@ -7,7 +7,7 @@ else()
 endif()
 
 #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -fivopts -flax-vector-conversions -funsafe-math-optimizations -Wall -dynamic")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -fivopts -flax-vector-conversions -funsafe-math-optimizations -Wall")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -g")
 
 if (NOT CMAKE_BUILD_TYPE MATCHES RELEASE)
