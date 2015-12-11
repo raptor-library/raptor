75a53a71762c3fbf78ed0a5a73ce759a5905bba2
mwang81 <mwang81@illinois.edu>
2015-12-11 16:03:53 -0600

    put in a mat mult test

---
+++ raptor/core/tests/test_mat_mult.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <mpi.h>
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "../par_vector.hpp"
+#include "../par_matrix.hpp"
+#include "../../gallery/matrix_IO.hpp"
+#include "../../gallery/stencil.hpp"
+#include "../../gallery/diffusion.hpp"
+
+#include "util/linalg/spmv.hpp"
+#include "util/linalg/matmult.hpp"
+
+TEST(core, test_mat_mult) {
+	MPI_Init(core, test_mat_mult);
+	int rank;
+	int comm_size;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
+	ASSERT_EQ(comm_size, 1);
+
+	using namespace raptor;
+
+	data_t vA[9] = {1,0,0,0,0,0,0,0,0};
+    data_t vx[15] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    data_t vBtest[15] = {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+    ParMatrix* A = new ParMatrix(3,3,vA);
+	ParMatrix* x = new ParMatrix(3,5,vx);
+	ParMatrix* B;
+	ParMatrix* Btest = new ParMatrix(3,5,vBtest);
+
+	parallel_matmult(A, x, &B);
+
+	MPI_Finalize();
+	ASSERT_EQ(B, Btest);
+}
