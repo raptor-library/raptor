b462c2f7df28994a537311434816284315da19fb
mwang81 <mwang81@illinois.edu>
2015-12-18 03:35:54 -0600

    manual mat mult test
    to run it, rename it to benchmark.cpp and run make

--- examples/benchmark_matmult.cpp
+++ examples/benchmark_matmult.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     int rank, num_procs;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
+    
     ParMatrix* matrices[2];
     ParMatrix* A;
 
