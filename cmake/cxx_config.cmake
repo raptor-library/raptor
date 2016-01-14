184f0de34095d9179a14fe83abbd51b45e498710
lukeolson <luke.olson@gmail.com>
2016-01-14 10:56:42 -0600

    first pass at hypre options

--- cmake/cxx_config.cmake
+++ cmake/cxx_config.cmake
@@ -6,14 +6,15 @@ else()
   message(FAIL_ERROR "A C++11 compiler is required.")
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -fivopts -flax-vector-conversions -funsafe-math-optimizations")
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -fivopts -flax-vector-conversions -funsafe-math-optimizations")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3")
 
-if (DEFINED ENV{HYPRE_LIB})
-    if (DEFINED ENV{MFEM_LIB} AND DEFINED ENV{METIS_LIB})
-        set(CMAKE_EXE_LINKER_FLAGS "-L$ENV{MFEM_LIB} -lmfem -L$ENV{METIS_LIB} -lmetis")
-    endif()
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L$ENV{HYPRE_LIB} -lHYPRE")
-endif()
+#if (DEFINED ENV{HYPRE_LIB})
+#    if (DEFINED ENV{MFEM_LIB} AND DEFINED ENV{METIS_LIB})
+#        set(CMAKE_EXE_LINKER_FLAGS "-L$ENV{MFEM_LIB} -lmfem -L$ENV{METIS_LIB} -lmetis")
+#    endif()
+#    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L$ENV{HYPRE_LIB} -lHYPRE")
+#endif()
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wall -llapack -lblas")
 
