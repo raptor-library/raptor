4d3e209dba67d02a82593f6677e3d8b03338b29e
Amanda Bienz <bienz2@illinois.edu>
2016-01-19 10:42:24 -0600

    Removed eigen folder.  Added metis/mfem cmake files

---
+++ cmake/FindMetis.cmake
@@ -0,0 +1,21 @@
+# from https://cmake.org/Wiki/CMake:How_To_Find_Libraries
+
+find_package(PkgConfig)
+pkg_check_modules(METIS QUIET metis)
+
+find_path(METIS_INCLUDE_DIR metis.h
+          HINTS ${PC_METIS_INCLUDEDIR} ${PC_METIS_INCLUDE_DIRS}
+			${METIS_DIR}/include)
+
+find_library(METIS_LIBRARY NAMES metis
+             HINTS ${PC_METIS_LIBDIR} ${PC_METIS_LIBRARY_DIRS}
+				${METIS_DIR}/libmetis ${METIS_DIR}/lib)
+
+set(METIS_LIBRARIES ${METIS_LIBRARY} )
+set(METIS_INCLUDE_DIRS ${METIS_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Metis  DEFAULT_MSG
+                                  METIS_LIBRARY METIS_INCLUDE_DIR)
+
+mark_as_advanced(METIS_INCLUDE_DIR METIS_LIBRARY )
---
+++ cmake/FindMfem.cmake
@@ -0,0 +1,21 @@
+# from https://cmake.org/Wiki/CMake:How_To_Find_Libraries
+
+find_package(PkgConfig)
+pkg_check_modules(MFEM QUIET mfem)
+
+find_path(MFEM_INCLUDE_DIR mfem.hpp
+          HINTS ${PC_MFEM_INCLUDEDIR} ${PC_MFEM_INCLUDE_DIRS}
+			${MFEM_DIR} ${MFEM_DIR}/include)
+
+find_library(MFEM_LIBRARY NAMES mfem
+             HINTS ${PC_MFEM_LIBDIR} ${PC_MFEM_LIBRARY_DIRS}
+				${MFEM_DIR} ${MFEM_DIR}/lib)
+
+set(MFEM_LIBRARIES ${MFEM_LIBRARY} )
+set(MFEM_INCLUDE_DIRS ${MFEM_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Mfem  DEFAULT_MSG
+                                  MFEM_LIBRARY MFEM_INCLUDE_DIR)
+
+mark_as_advanced(MFEM_INCLUDE_DIR MFEM_LIBRARY )
