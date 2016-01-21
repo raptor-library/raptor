969632269662034c3d3fb9cc5c1724794b7c423e
Amanda Bienz <bienz2@illinois.edu>
2016-01-21 09:26:31 -0600

    Updated the mfem wrapper to include all matrices in mfem folder

--- raptor/gallery/external/mfem_wrapper.hpp
+++ raptor/gallery/external/mfem_wrapper.hpp
@@ -7,7 +7,7 @@
 
 void mfem_laplace(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, const char* mesh_file, int order, bool visualization = false);
 
-/*void mfem_linear_elasticity(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, const char* mesh_file, int order, bool visualization = false);
+void mfem_linear_elasticity(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, const char* mesh_file, int order, bool visualization = false);
 
 void mfem_electromagnetic_diffusion(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, const char* mesh_file, int order, bool visualization = false);
 
@@ -15,6 +15,6 @@ void mfem_hdiv_diffusion(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, r
 
 void mfem_mixed_Darcy(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, const char* mesh_file, int order, bool visualization = false);
 
-void mfem_isoparametric_laplace(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, int elem_type, int ref_levels, int order, bool always_snap = false, bool visualization = false);*/
+void mfem_isoparametric_laplace(raptor::ParMatrix** A_ptr, raptor::ParVector** x_ptr, raptor::ParVector** b_ptr, int elem_type, int ref_levels, int order, bool always_snap = false, bool visualization = false);
 
 #endif
