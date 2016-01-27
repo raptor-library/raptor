b66fab9e50f5151e4841cdb3c5e1f5414207739c
Amanda <bienz2@illinois.edu>
2016-01-27 16:12:30 -0600

    Updated mfem benchmark

--- examples/benchmark_mfem.cpp
+++ examples/benchmark_mfem.cpp
@@ -14,20 +14,24 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    index_t num_tests = 1;
     char* mesh = argv[1];
-    int num_elements = atoi(argv[2]);
-
-    int order = 3;
-    if (argc > 3) order = atoi(argv[3]);
+    int num_tests = atoi(argv[2]);
+    int num_elements = atoi(argv[3]);
+    int order = atoi(argv[4]);
+    int async = atoi(argv[5]);
 
     ParMatrix* A;
     ParVector* x;
     ParVector* b;
 
-//    mfem_laplace(&A, &x, &b, mesh, order);
+//    mfem_laplace(&A, &x, &b, mesh, num_elements, order);
     mfem_linear_elasticity(&A, &x, &b, mesh, num_elements, order);
-//    mfem_electromagnetic_diffusion(&A, &x, &b, mesh, order);
+//    mfem_electromagnetic_diffusion(&A, &x, &b, mesh, num_elements, order);
+
+    long local_nnz = A->diag->nnz + A->offd->nnz;
+    long global_nnz;
+    MPI_Reduce(&local_nnz, &global_nnz, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("Num Nonzeros = %lu\n", global_nnz);
 
     data_t t0, tfinal;
 
@@ -35,30 +39,15 @@ int main(int argc, char *argv[])
     t0 = MPI_Wtime();
     for (int i = 0; i < num_tests; i++)
     {
-        parallel_spmv(A, x, b, 1.0, 0.0, 0);
+        parallel_spmv(A, x, b, 1.0, 0.0, async);
     }
     tfinal = (MPI_Wtime() - t0) / num_tests;
 
     // Print Timings
     MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("Max Time per Synchronous SpMV: %2.3e\n", t0);
+    if (rank == 0) printf("Max Time per (ASYNC=%d) SpMV: %2.3e\n", async, t0);
     MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("Avg Time per Synchronous SpMV: %2.3e\n", t0 / num_procs);
-
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    t0 = MPI_Wtime();
-    for (int i = 0; i < num_tests; i++)
-    {
-        parallel_spmv(A, x, b, 1.0, 0.0, 1);
-    }
-    tfinal = (MPI_Wtime() - t0) / num_tests;
-
-    // Print Timings
-    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("Max Time per ASYNC SpMV: %2.3e\n", t0);
-    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("Avg Time per ASYNC SpMV: %2.3e\n", t0 / num_procs);
+    if (rank == 0) printf("Avg Time per (ASYNC=%d) SpMV: %2.3e\n", async, t0 / num_procs);
 
     delete A;
     delete x;
