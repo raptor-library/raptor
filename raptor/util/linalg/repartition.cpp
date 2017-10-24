116d270686f5fd92b8364575429e584f6706912c
Amanda <bienz2@illinois.edu>
2017-10-24 16:12:51 -0500

    Updated repartition so ptscotch isnt needed

--- raptor/util/linalg/repartition.cpp
+++ raptor/util/linalg/repartition.cpp
@@ -1,91 +1,5 @@
 #include "repartition.hpp"
 
-int* ptscotch_partition(ParCSRMatrix* A)
-{
-    int rank, num_procs;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
-    // Variables for Graph Partitioning
-    SCOTCH_Num* partition = new SCOTCH_Num[A->local_num_rows + 2];
-    SCOTCH_Num baseval = 0; 
-    SCOTCH_Num vertlocnbr = A->local_num_rows;
-    SCOTCH_Num vertlocmax = A->local_num_rows;
-    SCOTCH_Num* vertloctab = new SCOTCH_Num[vertlocnbr + 2];
-    SCOTCH_Num* vendloctab = &vertloctab[1];
-    SCOTCH_Num* veloloctab = NULL;
-    SCOTCH_Num* vlblloctab = NULL;
-    SCOTCH_Num edgelocnbr = A->local_nnz;
-    SCOTCH_Num edgelocsiz = A->local_nnz;
-    SCOTCH_Num* edgeloctab = new SCOTCH_Num[edgelocsiz + 1];
-    SCOTCH_Num* edgegsttab = NULL;
-    SCOTCH_Num* edloloctab = NULL;
-
-    int row_start, row_end;
-    int idx, gbl_idx, ctr;
-    int err;
-
-    // Find matrix edge indices for PT Scotch
-    ctr = 0;
-    vertloctab[0] = 0;
-    for (int row = 0; row < A->local_num_rows; row++)
-    {
-        row_start = A->on_proc->idx1[row];
-        row_end = A->on_proc->idx1[row+1];
-        for (int j = row_start; j < row_end; j++)
-        {
-            idx = A->on_proc->idx2[j];
-            if (idx == row) continue;
-            gbl_idx = A->on_proc_column_map[idx];
-            edgeloctab[ctr] = gbl_idx;
-            ctr++;
-        }
-
-        if (A->off_proc_num_cols)
-        {
-            row_start = A->off_proc->idx1[row];
-            row_end = A->off_proc->idx1[row+1];
-            for (int j = row_start; j < row_end; j++)
-            {
-                idx = A->off_proc->idx2[j];
-                gbl_idx = A->off_proc_column_map[idx];
-                edgeloctab[ctr] = gbl_idx;
-                ctr++;
-            }
-        }
-        vertloctab[row+1] = ctr;
-    }
-    edgelocnbr = ctr;
-    edgelocsiz = ctr;
-   
-
-    SCOTCH_Dgraph dgraphdata;
-    SCOTCH_Strat stratdata;
-    SCOTCH_Arch archdata;
-
-    MPI_Comm comm;
-    MPI_Comm_dup(MPI_COMM_WORLD, &comm);
-
-    SCOTCH_dgraphInit(&dgraphdata, comm);
-    SCOTCH_dgraphBuild(&dgraphdata, baseval, vertlocnbr, vertlocmax,
-            vertloctab, vendloctab, veloloctab, vlblloctab, edgelocnbr, edgelocsiz, 
-            edgeloctab, edgegsttab, edloloctab);
-    SCOTCH_dgraphCheck(&dgraphdata);
-
-    SCOTCH_stratInit(&stratdata);
-    SCOTCH_dgraphPart(&dgraphdata, num_procs, &stratdata, partition);
-
-    SCOTCH_stratExit(&stratdata);
-    SCOTCH_dgraphExit(&dgraphdata);
-
-    delete[] vertloctab;    
-    delete[] edgeloctab;
-
-    MPI_Comm_free(&comm);
-
-    return partition;
-}
-
 void make_contiguous(ParCSRMatrix* A, const std::vector<int>& on_proc_column_map)
 {
     int rank;
@@ -784,11 +698,6 @@ ParCSRMatrix* repartition_matrix(ParCSRMatrix* A, int* partition, std::vector<in
     make_contiguous(A_part, A_part->on_proc_column_map);
 
     return A_part;
-}
-
-ParCSRMatrix* repartition_matrix(ParCSRMatrix* A, std::vector<int>& new_local_rows)
-{
-    return repartition_matrix(A, ptscotch_partition(A), new_local_rows);
 }
 
 
--- raptor/util/linalg/repartition.hpp
+++ raptor/util/linalg/repartition.hpp
@@ -3,7 +3,6 @@
 
 #include <mpi.h>
 #include "core/types.hpp"
-#include "ptscotch.h"
 #include <unistd.h>
 #include <set>
 #include "core/par_matrix.hpp"
@@ -11,9 +10,7 @@
 
 using namespace raptor;
 
-int* ptscotch_partition(ParCSRMatrix* A);
 ParCSRMatrix* repartition_matrix(ParCSRMatrix* A, int* partition, std::vector<int>& new_local_rows);
-ParCSRMatrix* repartition_matrix(ParCSRMatrix* A, std::vector<int>& new_local_rows);
 void make_noncontiguous(ParCSRMatrix* A, std::vector<int>& column_map);
 
 #endif
