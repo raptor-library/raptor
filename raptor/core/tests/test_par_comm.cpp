fb9ee0b648da2bb75ead9db1d797ff7bc430f11e
Amanda Bienz <bienz2@illinois.edu>
2017-11-02 11:49:01 -0500

    Fixed par_comm test

--- raptor/core/tests/test_par_comm.cpp
+++ raptor/core/tests/test_par_comm.cpp
@@ -19,47 +19,54 @@ int main(int argc, char* argv[])
     double eps = 0.001;
     double theta = M_PI / 8.0;
     int grid[2] = {10, 10};
+    int global_row, global_col;
+    int start, end;
+    double val;
     double* stencil = diffusion_stencil_2d(eps, theta);
+    std::vector<int> sendbuf;
+    std::vector<double> seq_row;
 
     CSRMatrix* A_seq = stencil_grid(stencil, grid, 2);
 
     ParCSRMatrix* A = par_stencil_grid(stencil, grid, 2);
 
     ParVector x(A->global_num_rows, A->local_num_rows, A->partition->first_local_row);
-    Vector& x_lcl = x.local;
-    for (int i = 0; i < A->local_num_rows; i++)
+    if (A->local_num_rows)
     {
-        x_lcl[i] = A->local_row_map[i];
+        sendbuf.resize(A->local_num_rows);
+        for (int i = 0; i < A->local_num_rows; i++)
+        {
+            sendbuf[i] = A->local_row_map[i];
+        }
     }
 
-    A->comm->communicate(x);
+    A->comm->communicate(sendbuf);
 
     assert(A->off_proc_num_cols > 0);
     for (int i = 0; i < A->off_proc_num_cols; i++)
     {
-        assert(fabs(A->comm->recv_data->buffer[i] - A->off_proc_column_map[i]) < zero_tol);
+        assert(A->comm->recv_data->int_buffer[i] == A->off_proc_column_map[i]);
     }
 
-    double A_dense[10000] = {0};
-    for (int i = 0; i < A_seq->n_rows; i++)
-    {
-        for (int j = A_seq->idx1[i]; j < A_seq->idx1[i+1]; j++)
-        {
-            A_dense[i*100 + A_seq->idx2[j]] = A_seq->vals[j];
-        }
-    }
-
+    seq_row.resize(A_seq->n_cols);
     CSRMatrix* recv_mat = A->comm->communicate(A);
     for (int i = 0; i < A->off_proc_num_cols; i++)
     {
-        int global_row = A->off_proc_column_map[i];
-        int row_start = recv_mat->idx1[i];
-        int row_end = recv_mat->idx1[i+1];
-        for (int j = row_start; j < row_end; j++)
+        global_row = A->off_proc_column_map[i];
+        start = A_seq->idx1[global_row];
+        end = A_seq->idx1[global_row+1];
+        for (int j = start; j < end; j++)
         {
-            int global_col = recv_mat->idx2[j];
-            double val = recv_mat->vals[j];
-            assert(fabs(A_dense[global_row*100 + global_col] - val) < zero_tol);
+            seq_row[A_seq->idx2[j]] = A_seq->vals[j];
+        }
+
+        start = recv_mat->idx1[i];
+        end = recv_mat->idx1[i+1];
+        for (int j = start; j < end; j++)
+        {
+            global_col = recv_mat->idx2[j];
+            val = recv_mat->vals[j];
+            assert(fabs(seq_row[global_col] - val) < 1e-06);
         }
     }
 
