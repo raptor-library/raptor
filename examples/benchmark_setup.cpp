2d9481d7b6672b409bdcb0663e2a192da6f7d46b
Amanda Bienz <bienz2@illinois.edu>
2017-12-13 12:35:39 -0600

    Added model times

--- examples/benchmark_setup.cpp
+++ examples/benchmark_setup.cpp
@@ -17,6 +17,8 @@
 #include "gallery/par_matrix_IO.hpp"
 #include "multilevel/par_multilevel.hpp"
 
+#define eager_cutoff 1000
+#define short_cutoff 62
 
 int main(int argc, char *argv[])
 {
@@ -154,9 +156,11 @@ int main(int argc, char *argv[])
     for (int i = 0; i < ml->num_levels; i++)
     {
         ParCSRMatrix* Al = ml->levels[i]->A;
+        ParVector& xl = ml->levels[i]->x;
 
         if (rank == 0) printf("Level %d\n", i);
 
+        int n_active;
         int num_msgs = Al->comm->send_data->num_msgs;
         if (Al->comm->recv_data->num_msgs > num_msgs)
             num_msgs = Al->comm->recv_data->num_msgs;
@@ -165,14 +169,78 @@ int main(int argc, char *argv[])
             size_msgs = Al->comm->recv_data->size_msgs;
         int has_comm = 0;
         if (num_msgs) has_comm = 1;
+        MPI_Allreduce(&has_comm, &n_active, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
+        int num_nodes = num_procs / 16;
+        int ppn = n_active / num_nodes;
+        if (ppn == 0) ppn = 1;
+
+
+        int n_short = 0;
+        int n_eager = 0;
+        int n_rend = 0;
+        int s_short = 0;
+        int s_eager = 0;
+        int s_rend = 0;
+
+
+        double short_a = 4.0e-6;
+        double eager_a = 1.1e-5;
+        double rend_a = 2.0e-5;
+        double short_b_inj = 6.3e8;
+        double short_b_max = -1.8e7;
+
+        double eager_b_inj = 1.7e9;
+        double eager_b_max = 6.2e7;
+
+        double rend_b_inj = 3.6e9;
+        double rend_b_max = 6.1e8;
+        double rend_b_n = 5.5e9;
+
+        double model = 0;
+        for (int i = 0; i < Al->comm->send_data->num_msgs; i++)
+        {
+            int start = Al->comm->send_data->indptr[i];
+            int end = Al->comm->send_data->indptr[i+1];
+            int size = end - start;
+            if (size < short_cutoff)
+            {
+                model += short_a + ((ppn * size) / (short_b_max + (ppn - 1)*short_b_inj));
+            }
+            else if (size < eager_cutoff)
+            {
+                model += eager_a + ((ppn * size) / (eager_b_max + (ppn - 1)*eager_b_inj));
+            }
+            else
+            {
+                double b_tmp = rend_b_max + (ppn-1)*rend_b_inj;
+                if (rend_b_n < b_tmp)
+                {
+                    b_tmp = rend_b_n;
+                }
+                model += rend_a + ((ppn*size) / b_tmp);
+            }
+        }
+
+        double comm_time = 0;
+        MPI_Barrier(MPI_COMM_WORLD);
+        t0 = MPI_Wtime();
+        for (int i = 0; i < 100; i++)
+        {
+            Al->comm->communicate(xl);
+        }
+        comm_time = (MPI_Wtime() - t0) / 100;
+        
         if (has_comm == 0) level_times[i] = 0.0;
         MPI_Reduce(&level_times[i], &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
         if (rank == 0) printf("Level Time: %e\n", t0);
+        MPI_Reduce(&model, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+        if (rank == 0) printf("Model Time: %e\n", t0);
+        MPI_Reduce(&comm_time, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+        if (rank == 0) printf("Measured Comm Time: %e\n", t0);
 
         int reduced;
-        MPI_Reduce(&has_comm, &reduced, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("Active Procs: %d\n", reduced);
+        if (rank == 0) printf("Active Procs: %d\n", n_active);
         MPI_Reduce(&num_msgs, &reduced, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
         if (rank == 0) printf("Max Num Msgs: %d\n", reduced);
         MPI_Reduce(&num_msgs, &reduced, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
