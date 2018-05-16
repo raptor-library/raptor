3364407561453bfc0e815b8e5d51e6ab054abfa3
Shelby Lockhart <sll2@illinois.edu>
2018-05-16 14:33:01 -0500

    Fixing Travis CI Build Issue

--- raptor/core/matrix.cpp
+++ raptor/core/matrix.cpp
@@ -864,7 +864,8 @@ void BSRMatrix::copy(const COOMatrix* A)
 
     std::vector<double> A_dense = A->to_dense();
 
-    double block_dense[n_rows*n_cols] = {0.0};
+    //double block_dense[n_rows*n_cols] = {0.0};
+    double block_dense[n_rows*n_cols];
 
     int block_ind, glob_i, glob_j;
     for (int k = 0; k < n_rows/b_rows; k++)
@@ -896,7 +897,8 @@ void BSRMatrix::copy(const CSRMatrix* A)
     n_cols = A->n_cols;
 
     std::vector<double> A_dense = A->to_dense();
-    double block_dense[n_rows*n_cols] = {0.0};
+    //double block_dense[n_rows*n_cols] = {0.0};
+    double block_dense[n_rows*n_cols];
 
     int block_ind, glob_i, glob_j;
     for (int k = 0; k < n_rows/b_rows; k++)
