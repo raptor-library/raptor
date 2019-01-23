1aa5faace2c1a37b91ca84f59602fbb81c3b5c80
Amanda Bienz <bienz2@bienz2.local>
2019-01-22 18:26:10 -0600

    Working on adding petsc interface

--- raptor/external/petsc_wrapper.cpp
+++ raptor/external/petsc_wrapper.cpp
@@ -6,7 +6,7 @@
 PetscErrorCode raptor_pc_create(stella_pc **pc)
 {
     raptor_shell* ctx;
-    cts = (raptor_shell*) malloc (sizeof(raptor_shell));
+    ctx = (raptor_shell*) malloc (sizeof(raptor_shell));
     *pc = ctx;
 
 	return 0;
@@ -17,28 +17,14 @@ PetscErrorCode raptor_pc_create(stella_pc **pc)
 #define __FUNCT__ "raptor_pc_setup"
 PetscErrorCode raptor_pc_setup(PC pc)
 {
-	#ifdef WITH_CEDAR
 	PetscErrorCode ierr;
 	Mat pmat;
-	stella_pc *pc_ctx;
-	stella_bmg_mat *mat_ctx;
-
+    raptor_pc *pc_ctx;
+    
 	ierr = PCGetOperators(pc,PETSC_NULL,&pmat);CHKERRQ(ierr);
 	ierr = PCShellGetContext(pc, (void**)&pc_ctx);CHKERRQ(ierr);
-	ierr = MatShellGetContext(pmat, (void**)&mat_ctx);CHKERRQ(ierr);
 
-	pc_ctx->nd = mat_ctx->nd;
-	pc_ctx->solvec = mat_ctx->solvec;
-	pc_ctx->rhsvec = mat_ctx->rhsvec;
-	int cedar_err;
-	cedar_err = cedar_solver_create(mat_ctx->so, &pc_ctx->solver);chkerr(cedar_err);
-
-	if (cedar_err) {
-		SETERRQ1(PETSC_COMM_WORLD, PETSC_ERR_LIB, "Cedar error code: %d", cedar_err);
-	}
-	#endif
-
-	return 0;
+	return ierr;
 }
 
 
@@ -46,26 +32,12 @@ PetscErrorCode raptor_pc_setup(PC pc)
 #define __FUNCT__ "raptor_pc_apply"
 PetscErrorCode raptor_pc_apply(PC pc, Vec x, Vec y)
 {
-	#ifdef WITH_CEDAR
-	int cedar_err;
 	PetscErrorCode ierr;
-	stella_pc *pc_ctx;
-	double *yarr;
-	const double *xarr;
+	raptor_shell *pc_ctx;
 
 	ierr = PCShellGetContext(pc, (void**)&pc_ctx);CHKERRQ(ierr);
-	ierr = VecGetArrayRead(x, &xarr);CHKERRQ(ierr);
-	ierr = VecGetArray(y, &yarr);CHKERRQ(ierr);
 
-	cedar_copyto(xarr, pc_ctx->rhsvec);
-	cedar_err = cedar_solver_run(pc_ctx->solver, pc_ctx->solvec, pc_ctx->rhsvec);chkerr(cedar_err);
-	cedar_copyfrom(pc_ctx->solvec, yarr);
-
-	ierr = VecRestoreArrayRead(x, &xarr);CHKERRQ(ierr);
-	ierr = VecRestoreArray(y, &yarr);CHKERRQ(ierr);
-	#endif
-
-	return 0;
+	return ierr;
 }
 
 
@@ -75,10 +47,10 @@ PetscErrorCode raptor_pc_apply(PC pc, Vec x, Vec y)
 PetscErrorCode raptor_pc_destroy(PC pc)
 {
 	PetscErrorCode ierr;
-	stella_pc *pc_ctx;
+	raptor_shell *pc_ctx;
 
 	ierr = PCShellGetContext(pc, (void**)&pc_ctx);CHKERRQ(ierr);
 	free(pc_ctx);
 
-	return 0;
+	return ierr;
 }
