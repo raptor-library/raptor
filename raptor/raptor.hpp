8b6d70e0dc67064c754d3cdd56bc52b8939ddce5
Amanda Bienz <bienz2@bienz2.local>
2019-01-28 12:51:07 -0600

    Added partial PETSc interface, adding full support soon

--- raptor/raptor.hpp
+++ raptor/raptor.hpp
@@ -44,12 +44,15 @@
     #include "gallery/par_matrix_market.hpp"
 #endif
 
-// Matrix external gallery
+// External 
 #ifdef USING_HYPRE
     #include "external/hypre_wrapper.hpp"
 #endif
 #ifdef USING_MFEM
     #include "external/mfem_wrapper.hpp"
+#endif
+#ifdef USING_PETSC
+    #include "external/petsc_wrapper.hpp"
 #endif
 
 // RugeStuben classes
