6cd7b0a379622903e21925443a1406e157441bf8
Shelby Lockhart <sll2@illinois.edu>
2019-03-05 09:47:11 -0600

    pushing everything for enlarged krylov, so it's up to date

--- raptor/krylov/par_cg.cpp
+++ raptor/krylov/par_cg.cpp
@@ -406,3 +406,178 @@ void SRECG(ParCSRMatrix* A, ParVector& x, ParVector& b, int t, aligned_vector<do
     delete W_temp;
     return;
 }
+
+void PSRECG(ParCSRMatrix* A, ParMultilevel *ml, ParVector& x, ParVector& b, int t, aligned_vector<double>& res, double tol, int max_iter)
+{
+    int rank, num_procs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    ParVector r;
+    ParVector z;
+    ParBVector *W = new ParBVector(A->global_num_cols, A->local_num_rows, A->partition->first_local_row, t);
+    ParBVector *W_temp = new ParBVector(A->global_num_cols, A->local_num_rows, A->partition->first_local_row, t);
+    ParBVector *Wk_1 = new ParBVector(A->global_num_cols, A->local_num_rows, A->partition->first_local_row, t);
+    ParBVector *Wk_2 = new ParBVector(A->global_num_cols, A->local_num_rows, A->partition->first_local_row, t);
+    ParVector Wa;
+    ParVector T;
+    Vector alpha;
+
+    int iter, recompute_r;
+    data_t rr_inner;
+    double norm_r;
+
+    // Adjust max iterations
+    if (max_iter <= 0)
+    {
+        max_iter = ((int)(1.3*b.global_n)) + 2;
+    }
+
+    // Fixed Constructors
+    r.resize(b.global_n, b.local_n, b.first_local);
+    z.resize(b.global_n, b.local_n, b.first_local);
+    Wa.resize(x.global_n, x.local_n, x.first_local);
+    T.resize(x.global_n, x.local_n, x.first_local);
+    alpha.resize(t);
+
+    // r0 = b - A * x0
+    A->residual(x, b, r);
+
+    // Append norm of initial residual to res
+    rr_inner = r.inner_product(r);
+    norm_r = sqrt(rr_inner);
+    res.emplace_back(norm_r);
+
+    // Adjust tolerance
+    if (norm_r != 0.0)
+    {
+        tol = tol * norm_r;
+    }
+
+    // Initial residual preconditioned before splitting
+    z.set_const_value(0.0);
+    ml->cycle(z, r);
+
+    // Perform first iteration outside loop
+    // to reduce control flow instructions
+    iter = 0;
+
+    // W = T(M^-1 * r0)
+    z.split_contig(*W, t);
+
+    // A-orthonormalize W
+    CGS(A, *W);
+    
+    // alpha = W^T * r
+    W->mult_T(r, alpha);
+
+    // x = x + W * alpha
+    W->mult(alpha, Wa);
+    x.axpy(Wa, 1.0);
+
+    // r = r - A * W * alpha
+    A->mult(Wa, T);
+    r.axpy(T, -1.0);
+
+    // Update norm of residual and iteration
+    rr_inner = r.inner_product(r);
+    norm_r = sqrt(rr_inner);
+    res.emplace_back(norm_r);
+    iter++;
+    
+    // Main SRECG Loop
+    while (norm_r > tol && iter < max_iter)
+    {
+        /*if (iter == 0)
+        {
+            // W = T(r0)
+            r.split_contig(W, t);
+
+            // A-orthonormalize W
+            CGS(A, W);
+        }
+        else
+        {*/
+            // Update Wk_1 and Wk_2
+            /*Wk_2.copy(Wk_1);
+            Wk_1.copy(W);
+
+            // W = A * W
+            A->mult(W, W_temp);*/
+
+            if (iter > 1)
+            {
+                // Update Wk_1 and Wk_2
+                Wk_2->copy(*Wk_1);
+                //Wk_2 = Wk_1;
+                Wk_1->copy(*W);
+                //Wk_1 = W;
+
+                // W = A * W
+                A->mult(*W, *W_temp);
+
+                // W = M^-1 * A * W
+
+                // A-orthonormalize W against previous vectors
+                BCGS(A, *Wk_1, *Wk_2, *W_temp);
+            }
+            else
+            {
+                // Update Wk_1
+                Wk_1->copy(*W);
+                //Wk_1 = W;
+
+                // W = A * W
+                A->mult(*W, *W_temp);
+                
+                // W = M^-1 * A * W
+
+                // A-orthonormalize W against previous vectors
+                BCGS(A, *Wk_1, *W_temp);
+            }
+
+            W->copy(*W_temp);
+            //W = W_temp;
+
+            // A-orthonormalize W
+            CGS(A, *W);
+        //}
+
+        // alpha = W^T * r
+        W->mult_T(r, alpha);
+
+        // x = x + W * alpha
+        W->mult(alpha, Wa);
+        x.axpy(Wa, 1.0);
+
+        // r = r - A * W * alpha
+        A->mult(Wa, T);
+        r.axpy(T, -1.0);
+
+        // Update norm of residual and iteration
+        rr_inner = r.inner_product(r);
+        norm_r = sqrt(rr_inner);
+        res.emplace_back(norm_r);
+        iter++;
+    }
+
+    /*if (rank == 0)
+    {
+        if (iter == max_iter)
+        {
+            printf("Max Iterations Reached.\n");
+            printf("2 Norm of Residual: %lg\n\n", norm_r);
+        }
+        else
+        {
+            printf("%d Iteration required to converge\n", iter);
+            printf("2 Norm of Residual: %lg\n\n", norm_r);
+        }
+    }*/
+
+    delete W;
+    delete Wk_1;
+    delete Wk_2;
+    delete W_temp;
+    return;
+}
