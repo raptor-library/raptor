019ce3b6100f260ae37587d0c32afe45194b6d7f
Andrew Reisner <andrew@reisner.co>
2026-06-15 11:01:55 -0400

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "RAPtor algebraic multigrid library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "raptor";
+          version = "0.4";
+
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
+
+          buildInputs = [
+            pkgs.mpi
+            pkgs.blas
+            pkgs.lapack
+            pkgs.hypre
+          ];
+
+          cmakeFlags = [
+            "-DWITH_MPI=ON"
+            "-DWITH_HYPRE=ON"
+            "-DHYPRE_DIR=${pkgs.hypre}"
+            "-DENABLE_UNIT_TESTS=OFF"
+            "-DBUILD_EXAMPLES=OFF"
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.cmake
+            pkgs.ninja
+            pkgs.pkg-config
+            pkgs.mpi
+            pkgs.blas
+            pkgs.lapack
+            pkgs.hypre
+            pkgs.gtest
+          ];
+        };
+      });
+}
