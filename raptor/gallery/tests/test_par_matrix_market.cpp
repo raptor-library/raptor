f608d12c23020d2a5a80d8a89440f410345a9446
Luke Olson <luke.olson@gmail.com>
2026-07-16 21:22:53 -0500

    add debug info to ci

--- raptor/gallery/tests/test_par_matrix_market.cpp
+++ raptor/gallery/tests/test_par_matrix_market.cpp
@@ -36,10 +36,10 @@ TEST(ParAnisoTest, TestsInGallery)
     MPI_Barrier(MPI_COMM_WORLD);
     compare(Amm, Amm_out);
 
-    // Diff the two mtx files 
+    // Diff the two mtx files
     if (rank == 0)
     {
-        remove(f_out);
+        // remove(f_out);  // DEBUG: keep f_out for CI inspection
     }
 
     delete Amm_out;
